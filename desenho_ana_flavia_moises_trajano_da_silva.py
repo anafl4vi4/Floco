448729d2bc80f3802fc062cb64d2d05f06595cbc
Ana Flávia <fabiolamanu1989@gmail.com>
2024-12-12 09:54:19 -0300

    Update desenho_ana_flavia_moises_trajano_da_silva.py

--- desenho_ana_flavia_moises_trajano_da_silva.py
+++ desenho_ana_flavia_moises_trajano_da_silva.py
@@ -1,32 +1,26 @@
 import turtle
-t = turtle 
-t.pensize(5)
-c: str = t.color
-t.speed(3)
-
-# 4 segmentos de reta
+t = turtle
+c = t.color
 def losango(tamanho, zoom: int = 1, posx=1, posy=1):
-    t.up()
     c("navy")
-    t.goto(posx*60, posy*60) 
-    t.down()
-
+    turtle.penup()
+    turtle.goto(posx*60, posy*60)
+    turtle.pendown()
     for _ in range(2):
-        t.fd(tamanho*zoom)
+        turtle.forward(tamanho*zoom)
         turtle.left(60)
-        t.fd(tamanho*zoom)
-        t.left(120)
+        turtle.forward(tamanho*zoom)
+        turtle.left(120)
         pass
-    pass
-
-# Os args zoom, x e y também deveriam aparecer aqui
-def flor_losango(t_losango):
+        
+def floco(tamanho_losango):
+    turtle.speed(2)
     for _ in range(5):
-        losango(t_losango)
-        t.right(72)
+        losango(zoom=9, tamanho=20)
+        losango(tamanho_losango)
+        turtle.right(72)  
         pass
-input("pressione enter ")
 
-flor_losango(150)
-losango(30, 0.5, -50, -50)
-t.done()
+floco (100)
+
+turtle.done
